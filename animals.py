95012b5af7d5a20a101f71e3882ca4e2e7c70524
Olga Zhuravleva <olya-8507@mail.ru>
2018-09-11 23:46:27 +0300

    Животные начало

--- animals.py
+++ animals.py
@@ -1,3 +1,47 @@
-print('Привет мир')
-print('Привет мир')
-print('Привет мир')
+class Tetrapoda:
+  spine = true
+  leg = 4
+class Birds(Tetrapoda):
+  plumage = true
+  child = egg
+class Theria(Tetrapoda):
+  child = viviparous
+  children_food = milk
+class Home_Animals:
+  locality = farme
+  feed = true
+class Cow(Theria, Home_Animals):
+  weight > 350 #кг
+  voice = 'му му'
+  use =cow_milk
+  feed = hay
+class Goat(Theria, Home_Animals):
+ 35 < weight < 50 #кг
+ voice = 'ме ме'
+ use = goat_milk
+ feed = hay
+class  Sheep (Theria, Home_Animals):
+  45 < weight < 100 #кг
+  voice = 'бе бе'
+  use = wool
+  feed = hay
+class  Pig (Theria, Home_Animals):
+  150 < weight < 300 #кг
+  voice = 'бе бе'
+  use = meat
+  feed = forage
+class  Duck (Birds, Home_Animals):
+  1,5 < weight < 3 #кг
+  voice = 'кря кря'
+  use = meat
+  feed = mixed_fodder
+class  Goose (Birds, Home_Animals):
+  5 < weight < 10 #кг
+  voice = 'га га'
+  use = meat
+  feed = mixed_fodder
+class  Goose (Birds, Home_Animals):
+  2 < weight < 5 #кг
+  voice = 'ко ко'
+  use = egg
+  feed = mixed_fodder
